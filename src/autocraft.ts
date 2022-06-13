327ff0e11ad8908f998375a30dd956bbdb901182
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2022-06-13 03:28:50 -0500

    Update autocraft.ts
    Refactor

--- src/autocraft.ts
+++ src/autocraft.ts
@@ -1,22 +1,34 @@
-import { FormLib } from "DMLib"
+import { FormLib, Hotkeys } from "DMLib"
 import { Autocraft } from "items"
 import { mcm } from "shared"
 import { FurnitureEvent, ObjectReference } from "skyrimPlatform"
 
-const ObjRefHasName = (f: ObjectReference, name: string) =>
-  f.getBaseObject()?.getName().toLowerCase().includes(name)
-
 const IsPlayer = (f: ObjectReference) => f.getFormID() === FormLib.playerId
-const IsAlchemyLab = (f: ObjectReference) => ObjRefHasName(f, "alchemy")
+const IsAlchemyLab = FormLib.IsAlchemyLab
+
+interface ActionFunctions {
+  Alchemy: Hotkeys.KeyPressEvt
+  Enchanting: Hotkeys.KeyPressEvt
+  Smithing: Hotkeys.KeyPressEvt
+}
 
 export function AutocraftEnter(e: FurnitureEvent) {
-  if (!IsPlayer(e.actor)) return
-  if (IsAlchemyLab(e.target) && mcm.autocraft.alchemy)
-    Autocraft.Ingredients.GetFrom()
+  FurnitureAction(e, {
+    Alchemy: Autocraft.Ingredients.GetFrom,
+    Enchanting: Hotkeys.DoNothing,
+    Smithing: Hotkeys.DoNothing,
+  })
 }
 
 export function AutocraftExit(e: FurnitureEvent) {
+  FurnitureAction(e, {
+    Alchemy: Autocraft.Ingredients.SendTo,
+    Enchanting: Hotkeys.DoNothing,
+    Smithing: Hotkeys.DoNothing,
+  })
+}
+
+function FurnitureAction(e: FurnitureEvent, a: ActionFunctions) {
   if (!IsPlayer(e.actor)) return
-  if (IsAlchemyLab(e.target) && mcm.autocraft.alchemy)
-    Autocraft.Ingredients.SendTo()
+  if (IsAlchemyLab(e.target) && mcm.autocraft.alchemy) a.Alchemy()
 }
